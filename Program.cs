8f4615e04cc7e0e7565848cd59dd6347ce586b9f
Rafael C. Silva <rafael.silva623@fatec.sp.gov.br>
2022-03-09 15:56:58 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.jogo_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
